ea074ea482e90bfacd580b28ef01ff77ac7dfb3f
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2022-04-20 17:18:46 -0700

    branch feat

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+import HelloWorld from './components/HelloWorld.vue';
+const number = ref(5);
+const count = ref(12);
+const stuff = computed(() => {
+  return number.value * 2;
+});
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ stuff }}</h1>
+  <HelloWorld :count="count" />
+  <button @click="number++">+</button>
+  <button @click="number--">-</button>
+  <h1>{{ count }}</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: String,
+  count: Number,
+});
+</script>
+
+<template>
+  <button @click="$emit(count++)">Tester</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
